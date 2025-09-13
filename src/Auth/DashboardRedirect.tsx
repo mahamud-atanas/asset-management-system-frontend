5bdc2a71489dc46c05ecc5a54648e80aa92e571d
mahamud <89572296+mahamud-atanas@users.noreply.github.com>
2025-09-13 23:56:12 +0300

    Update DashboardRedirect.tsx

--- src/Auth/DashboardRedirect.tsx
+++ src/Auth/DashboardRedirect.tsx
@@ -4,19 +4,17 @@ import { Navigate } from "react-router-dom";
 const DashboardRedirect = () => {
   const { user, isAuthenticated } = useAuth();
 
-  if (!isAuthenticated()) {
-    return <Navigate to="/login" />;
-  }
+  if (!isAuthenticated()) return <Navigate to="/login" replace />;
 
   switch (user?.role) {
     case "admin":
-      return <Navigate to="/admin" />;
+      return <Navigate to="/admin" replace />;
     case "superadmin":
-      return <Navigate to="/superadmin" />;
+      return <Navigate to="/superadmin" replace />;
     case "user":
-      return <Navigate to="/user" />;
+      return <Navigate to="/admin/viewAsset" replace />; // or keep /user if you have UserLayout
     default:
-      return <Navigate to="/unauthorized" />;
+      return <Navigate to="/unauthorized" replace />;
   }
 };
 
