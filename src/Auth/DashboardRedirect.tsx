94f7801f2a09de6ca7664d1d04c0ae1783ef3866
mahamud <89572296+mahamud-atanas@users.noreply.github.com>
2025-09-14 19:38:53 +0300

    Update DashboardRedirect.tsx

--- src/Auth/DashboardRedirect.tsx
+++ src/Auth/DashboardRedirect.tsx
@@ -12,7 +12,7 @@ const DashboardRedirect = () => {
     case "superadmin":
       return <Navigate to="/superadmin" replace />;
     case "user":
-      return <Navigate to="/admin/viewAsset" replace />; // or keep /user if you have UserLayout
+      return <Navigate to="/user" replace />; // or keep /user if you have UserLayout
     default:
       return <Navigate to="/unauthorized" replace />;
   }
